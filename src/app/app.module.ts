757eb13876e9de9e6c03b624a6a575e7ebe72194
Misko183 <rtmike183@gmail.com>
2022-01-24 21:19:54 +0100

    Login without future for admin web view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { ItemsComponent } from './items/items.component'; // <-- NgModel lives here
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { ItemsComponent } from './items/items.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { FreeItemsComponent } from './free-items/free-items.component';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
-import { ItemSearchComponent } from './item-search/item-search.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { ItemSearchComponent } from './item-search/item-search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import { MatButton, MatButtonModule } from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { MatInputModule} from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeroFormComponent } from './hero-form/hero-form.component';
+import { CommonModule } from '@angular/common';
 import { ItemFormComponent } from './item-form/item-form.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenu, MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FreeItemsComponent } from './free-items/free-items.component';
+
 
 @NgModule({
   declarations: [
@@ -43,6 +51,10 @@ import { UserComponent } from './user/user.component';
     UserComponent,
   ],
   imports: [
+    MatInputModule,
+    MatToolbarModule,
+    CommonModule,
+    MatSelectModule,
     BrowserModule,
     FormsModule,
     AppRoutingModule,
@@ -53,8 +65,11 @@ import { UserComponent } from './user/user.component';
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
-    MatFormFieldModule,
     MatIconModule,
+    MatMenuModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatAutocompleteModule
   ],
 
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  username: string;
+  password: string;
+
+  login(): void{
+    if((this.username == "admin" && this.password == "admin") || (this.username=="user" && this.password=="user")){
+      //this.router.navigate(["user"]);
+      this.router.navigate(["dashboard"]);
+    }
+    else{
+      alert("Username or password are invalid")
+    }
+  }
 }
+
